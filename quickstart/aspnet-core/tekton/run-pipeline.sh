beeb61dc17c6eb940433333104607e32d456dac0
Kaspars Skels <kaspars.skels@gmail.com>
2021-06-09 16:13:25 -0500

    Minor fix for aspnet-core pipeline files
    Signed-off-by: Kaspars Skels <kaspars.skels@gmail.com>

--- quickstart/aspnet-core/tekton/run-pipeline.sh
+++ quickstart/aspnet-core/tekton/run-pipeline.sh
@@ -1,5 +1,5 @@
 tkn pipeline start build-and-deploy \
-    -w name=shared-workspace,volumeClaimTemplateFile=03_persistent_volume_claim.yaml \
+    -w name=shared-workspace,volumeClaimTemplateFile=04_persistent_volume_claim.yaml \
     -p deployment-name=aspnet-core-app \
     -p git-url=https://github.com/kskels/azure-cache-redis-samples.git \
     -p IMAGE=image-registry.openshift-image-registry.svc:5000/dotnet-demo/aspnet-core-app
