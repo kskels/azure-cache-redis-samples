126d611dce4bce29c34fce58b42b2ba55a8e9b3e
Kaspars Skels <kaspars.skels@gmail.com>
2021-06-11 12:10:27 -0500

    Updated tekton pipeline for aspnet-core app
    Signed-off-by: Kaspars Skels <kaspars.skels@gmail.com>

--- quickstart/aspnet-core/tekton/run-pipeline.sh
+++ quickstart/aspnet-core/tekton/run-pipeline.sh
@@ -1,5 +1,5 @@
-tkn pipeline start build-and-deploy \
-    -w name=shared-workspace,volumeClaimTemplateFile=04_persistent_volume_claim.yaml \
+tkn pipeline start dotnet-docker-build-and-deploy \
+    -w name=shared-workspace,volumeClaimTemplateFile=pvc-source.yaml \
     -p deployment-name=aspnet-core-app \
     -p git-url=https://gitlab.kskels.com/demos/azure-cache-redis-samples.git \
     -p IMAGE=image-registry.openshift-image-registry.svc:5000/dotnet-demo/aspnet-core-app
